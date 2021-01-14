2bef6f80117eb2616a3e89eabda719288096d70d
Raafi Hilmi <raafihilmi90@gmail.com>
2021-01-14 18:24:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const deadline = '2021-1-29';
+const getTimeRemaining = (endtime) => {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  return {
+    total,
+    seconds,
+    minutes,
+    hours,
+    days,
+  };
+};
+const initializeClock = (endtime) => {
+  const daysSpan = document.getElementById('days');
+  const hoursSpan = document.getElementById('hours');
+  const minutesSpan = document.getElementById('minutes');
+  const secondsSpan = document.getElementById('seconds');
+  const border = document.getElementById('border');
+  const updateClock = () => {
+    const t = getTimeRemaining(endtime);
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    border.classList.toggle('flip');
+
+    if (t.total <= 0) {
+      clearInterval(timeInterval);
+    }
+  };
+
+  updateClock();
+  const timeInterval = setInterval(updateClock, 1000);
+};
+
+initializeClock(deadline);
